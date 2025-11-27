66da5c2708ac58ded4f3f3b097c7f7b35112431b
Devaprem1996 <devaprem10@gmail.com>
2025-11-27 14:00:42 +0530

    router.js file edit for 404 page error

--- api/config.js
+++ api/config.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
-    const url = process.env.SUPABASE_URL;
-    const anonKey = process.env.SUPABASE_ANON_KEY;
+    const url = "https://bovtrdrmivmyglafpojx.supabase.co"
+    const anonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJvdnRyZHJtaXZteWdsYWZwb2p4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwMDk0MzcsImV4cCI6MjA3OTU4NTQzN30.P2eUq0MxyvX_5O0i4B1gjwjcvjbf9vy5lYl05AETHx8";
 
     if (!url || !anonKey) {
         res.status(500).json({
