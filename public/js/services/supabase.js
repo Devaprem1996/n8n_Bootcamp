fcfc62c49e7927962ec8ab07f79931d64b2ea91a
Devaprem1996 <devaprem10@gmail.com>
2025-11-27 14:06:50 +0530

    api issue

--- public/js/services/supabase.js
+++ public/js/services/supabase.js
@@ -5,8 +5,9 @@
 
 let supabase = null;
 let supabaseConfigPromise = null;
-SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJvdnRyZHJtaXZteWdsYWZwb2p4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwMDk0MzcsImV4cCI6MjA3OTU4NTQzN30.P2eUq0MxyvX_5O0i4B1gjwjcvjbf9vy5lYl05AETHx8"
-SUPABASE_URL = "https://bovtrdrmivmyglafpojx.supabase.co"
+
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJvdnRyZHJtaXZteWdsYWZwb2p4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQwMDk0MzcsImV4cCI6MjA3OTU4NTQzN30.P2eUq0MxyvX_5O0i4B1gjwjcvjbf9vy5lYl05AETHx8"
+const SUPABASE_URL = "https://bovtrdrmivmyglafpojx.supabase.co"
 // -------------------------------
 // LOAD SUPABASE CONFIG
 // -------------------------------
